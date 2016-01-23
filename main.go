3ed4e4cdb2d0e560650a633afccbfeca0acfdef5
Gaurav Mittal <gauravkm@gmail.com>
2016-01-23 14:10:48 -0800

    Crude version of shortening url

--- main.go
+++ main.go
@@ -1,15 +1,48 @@
 package main
 
 import (
+	"code.google.com/p/go-uuid/uuid"
 	"fmt"
 	"net/http"
 )
 
+var lookup map[string]string = make(map[string]string)
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello world", r.URL.Path[1:])
+	longUrl := r.URL.Path[9:]
+	//Do some validation on the long url
+	shortUrl := shortenUrl(longUrl)
+
+	fmt.Fprintf(w, "shortened Url", shortUrl)
+}
+
+func lookupHandler(w http.ResponseWriter, r *http.Request) {
+	shortUrl := r.URL.Path[8:]
+	//TODO: Validate that the short url is non null
+	longUrl := lookItUp(shortUrl)
+	fmt.Fprintf(w, "output: ", longUrl)
+}
+
+func shortenUrl(longUrl string) string {
+	shortUrl := createUniqueMapping(longUrl)
+	lookup[shortUrl] = longUrl
+	return shortUrl
+}
+
+func createUniqueMapping(longUrl string) string {
+	id := uuid.NewUUID()
+	return id.String()[:7]
+}
+func lookItUp(shortUrl string) string {
+	if longUrl := lookup[shortUrl]; longUrl != "" {
+		return longUrl
+	} else {
+		return "unable to lookup: " + shortUrl
+	}
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/shorten/", handler)
+	http.HandleFunc("/lookup/", lookupHandler)
 	http.ListenAndServe(":8800", nil)
 }
